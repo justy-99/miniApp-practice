79a4c00487597032d0b1053e7e5eca176957d864
suzhenyi <suzhenyi1999@163.com>
2022-08-13 12:42:16 +0800

    基本语法复习

--- pages/favor/favor.js
+++ pages/favor/favor.js
@@ -1,66 +1,33 @@
 // pages/favor/favor.js
+// 实例化: 页面实例
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    // 1.案例一: 数据绑定 mustache语法
+    message: "Hello World",
 
+    // 2.案例二: 列表数据
+    movies: ["少年派", "大话西游", "星际穿越", "独行月球"],
+
+    // 3.案例三: 计数器
+    counter: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  // 监听的事件方法
+  increment() {
+    console.log("+1");
+    // 修改data中的数据, 但是你的修改并不会引起页面的刷新(自动检测你的新数据重新渲染页面, 在小程序中不会, 在react中也不会)
+    // this.data.counter += 1
+    // console.log(this.data.counter);
 
+    // 修改data, 并且希望页面重新渲染, 这里必须使用this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  decrement() {
+    console.log("-1");
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
