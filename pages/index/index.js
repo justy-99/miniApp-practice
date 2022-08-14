7277604c128a225d459c52a42a83605a9efc9bc0
suzhenyi <suzhenyi1999@163.com>
2022-08-14 20:18:03 +0800

    api、路由练习

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,7 +10,9 @@ Page({
       { name: "06_学习event", path: "/pages/06_learn_event/index" },
       { name: "07_学习组件化", path: "/pages/07_learn_cpns/index" },
       { name: "08_学习组件化2", path: "/pages/08_learn_slot/index" },
-      { name: "09_学习网络请求", path: "/pages/09_learn_network/index" }
+      { name: "09_学习网络请求", path: "/pages/09_learn_network/index" },
+      { name: "10_学习系统API", path: "/pages/10_learn_api/index" },
+      { name: "11_学习页面导航", path: "/pages/11_learn_nav/index" },
 
     ]
   },
